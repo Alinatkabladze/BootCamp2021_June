c9ad698359ae8fe7af41e60b1550e63c93c20657
MaChankv <43888116+MaChankv@users.noreply.github.com>
2021-07-31 00:03:33 +0400

    Add SQL Connection Setup

---
+++ src/test/java/DDT/SQLJDBCUtil.java
@@ -0,0 +1,56 @@
+package DDT;
+
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ *
+ * @author mysqltutorial.org
+ */
+public class SQLJDBCUtil {
+
+    /**
+     * Get database connection
+     *
+     * @return a Connection object
+     * @throws SQLException
+     */
+    public static Connection getConnection() throws SQLException {
+        Connection conn = null;
+
+        try (FileInputStream f = new FileInputStream("src/main/resources/db.properties")) {
+
+            // load the properties file
+            Properties pros = new Properties();
+            pros.load(f);
+
+            // assign db parameters
+            String url = pros.getProperty("url");
+            String user = pros.getProperty("user");
+            String password = pros.getProperty("password");
+
+            // create a connection to the database
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+    @Test
+    public void db() throws SQLException {
+        Connection conn = SQLJDBCUtil.getConnection();
+
+        // print out a message
+        System.out.println(String.format("Connected to database %s "
+                + "successfully.", conn.getCatalog()));
+
+    }
+
+}
+
